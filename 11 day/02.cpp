eb93a35014f6219b1e4ceebc1669fc7b54f4ccdf
Prince Kumar <princeinfo053@gmail.com>
2024-04-07 09:57:04 +0000

    2nd pattern problem solved

---
+++ 11 day/02.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()  {
+    int n;
+    cin>>n;
+
+    for(int i =1; i<=n; i++) {
+
+        //print space
+        int space = n-i;
+        while (space) {
+            cout<<"  ";
+            space--;
+        }
+
+        //print number
+        for (int j = 1; j <= (2*i - 1); j++) {
+            cout<<j<<" ";
+        }
+
+        cout<<endl;
+    }
+}
