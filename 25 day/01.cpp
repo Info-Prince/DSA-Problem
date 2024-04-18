124f705633899904a41d57e61e36627442c459dc
Prince Kumar <princeinfo053@gmail.com>
2024-04-18 11:59:00 +0000

    selection sort

---
+++ 25 day/01.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main ()     {
+    int n;
+    cout<<"Enter size of array: ";
+    cin>>n;
+
+    cout<<"Enter elements in array: ";
+    int arr [1000];
+    for (int i =0; i<n; i++)
+    cin>>arr[i];
+
+
+    // selection sort algorithm
+    for (int i =0; i<n-1; i++) {
+        int index = i;
+        for (int j = i+1; j<n; j++) {
+            if (arr[j] < arr[j-1]) 
+            index = j;
+        }
+        swap (arr[index], arr[i]);
+    }
+
+    //print sorted array
+    for (int i =0; i<n; i++)
+    cout<<arr[i]<<" ";
+}
